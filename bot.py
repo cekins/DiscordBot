41d482199dd8c78971a456b4430cc4a0974960d8
BuildTools <unconfigured@null.spigotmc.org>
2020-01-05 01:40:14 -0500

    finished first version

--- bot.py
+++ bot.py
@@ -1,20 +1,35 @@
 import os
+import time
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+os.environ['Path'] += os.pathsep + os.path.join(os.path.dirname(os.path.realpath(__file__)), 'ffmpeg\\bin')
 
-@client.event
+bot = commands.Bot(command_prefix="!")
+
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print('Bot Ready')
 
-@client.event
-async def on_message(message):
-    if message.content =='test':
-        print("got it")
 
-client.run(token)
+@bot.command()
+async def run(ctx):
+    vchan = ctx.message.author.voice.channel
+    vcon = await vchan.connect()
+    vcon.play(discord.FFmpegPCMAudio('audio/run.mp3'))
+    while vcon.is_playing():
+        pass
+    await vcon.disconnect()
+
+
+
+
+
+
+bot.run(token)
